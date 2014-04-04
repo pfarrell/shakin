581db1d8b36aa4f33614f4ac0edcd65673038be8
Pat Farrell <pfarrell@ulive.com>
2014-04-04 01:14:43 -0700

    wrapper for json output

---
+++ lib/quake_feed/record_set.rb
@@ -0,0 +1,34 @@
+require 'date'
+require 'csv'
+#require 'active_support/core_ext/object'
+
+class RecordSet
+  attr_accessor :date, :data, :errors, :next, :prev, :uri
+
+  def initialize(opts={})
+    @date=DateTime.now
+    @errors=[]
+    @properties=[]
+
+    opts.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
+
+  def to_json(options = {})
+    return {date: @date, errors: @errors, data: @data, properties: @properties, params: @params, prev: @prev, uri: @uri, next: @next}.to_json
+  end
+
+  def to_csv(options = {})
+    csv_opts = { headers: @properties, write_headers: true }
+
+    csv_string = CSV.generate(csv_opts) do |csv|
+      @data.each do |row|
+        # make sure we add the row data in the same order as the headers
+        csv << properties.collect { |p| row[p] }
+      end
+    end
+
+    return csv_string
+  end
+end
