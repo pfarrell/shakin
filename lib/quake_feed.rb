f20c02bd7359415e9a773156425122ec1da55acd
Pat Farrell <pfarrell@ulive.com>
2014-04-05 10:39:40 -0700

    haversine and location support

--- lib/quake_feed.rb
+++ lib/quake_feed.rb
@@ -1,6 +1,8 @@
 require "quake_feed/version"
 require "quake_feed/quake"
 require "quake_feed/record_set"
+require "quake_feed/location"
+require "quake_feed/haversine"
 
 module QuakeFeed
   # Your code goes here...
