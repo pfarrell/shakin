c491efd9c8272fd7e49bbbb596790584f31f6c85
Pat Farrell <pfarrell@ulive.com>
2014-04-06 23:11:08 -0700

    json checks

---
+++ spec/quake_feed/record_set_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe RecordSet do
+end
--- spec/shakin_spec.rb
+++ spec/shakin_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'byebug'
 
 describe 'Shakin, a Sinatra joint,' do
 
@@ -7,4 +8,15 @@ describe 'Shakin, a Sinatra joint,' do
     expect(last_response).to be_ok
   end
 
+  it "should return html to /earthquakes" do
+    get "/earthquakes"
+    expect(last_response).to be_ok
+    expect(last_response.header["content-type"]).to eq("text/html;charset=utf-8")
+  end
+
+  it "should return json for /earthquakes.json" do
+    get "/earthquakes.json"
+    expect(last_response).to be_ok
+    expect(last_response.header["content-type"]).to eq("application/json;charset=utf-8")
+  end
 end
