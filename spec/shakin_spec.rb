fe8474d3f76890490d5324990b2406f738072613
Pat Farrell <pfarrell@ulive.com>
2014-04-07 13:06:50 -0700

    updated spec with new url versioning scheme

--- spec/shakin_spec.rb
+++ spec/shakin_spec.rb
@@ -8,14 +8,14 @@ describe 'Shakin, a Sinatra joint,' do
     expect(last_response).to be_ok
   end
 
-  it "should return html to /earthquakes" do
-    get "/earthquakes"
+  it "should return html to /v1/earthquakes" do
+    get "/v1/earthquakes"
     expect(last_response).to be_ok
     expect(last_response.header["content-type"]).to eq("text/html;charset=utf-8")
   end
 
   it "should return json for /earthquakes.json" do
-    get "/earthquakes.json"
+    get "/v1/earthquakes.json"
     expect(last_response).to be_ok
     expect(last_response.header["content-type"]).to eq("application/json;charset=utf-8")
   end
