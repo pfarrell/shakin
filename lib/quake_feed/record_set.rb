e25f5bd26f1d7f3691e752b07c85873f04ce884e
Pat Farrell <pfarrell@ulive.com>
2014-04-10 17:20:17 -0700

    covered record_set

--- lib/quake_feed/record_set.rb
+++ lib/quake_feed/record_set.rb
@@ -19,16 +19,4 @@ class RecordSet
     return {date: @date, errors: @errors, data: @data}.to_json
   end
 
-  def to_csv(options = {})
-    csv_opts = { headers: @properties, write_headers: true }
-
-    csv_string = CSV.generate(csv_opts) do |csv|
-      @data.each do |row|
-        # make sure we add the row data in the same order as the headers
-        csv << properties.collect { |p| row[p] }
-      end
-    end
-
-    return csv_string
-  end
 end
